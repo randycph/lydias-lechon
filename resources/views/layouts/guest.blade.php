64448031dab08f9fce79efc2757e6149ca5c9ca6
randycph <randycph@gmail.com>
2025-03-19 09:36:11 +0800

    frontend update

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -34,7 +34,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 </head>
-<body x-cloak class="bg-gray-100 text-gray-900" x-data="{ open: false, openCart: false }">
+<body x-cloak class="bg-gray-100 text-gray-900" x-data="{ open: false, openCart: false, marketingPopup: false, openHotline: true }">
     <nav  class="fixed top-5 left-1/2 transform -translate-x-1/2 w-[90%] bg-green-700 px-4 py-3 flex justify-between items-center z-50 rounded-full">
         <button @click="open = true">
             <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 text-white cursor-pointer">
@@ -102,11 +102,11 @@
             x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0"
             x-transition:leave="transition-transform transform duration-300"
             x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full"
-            class="fixed top-[5%] right-[5%] w-[90%] h-[90%] bg-white text-black z-50 py-6 flex flex-col shadow-lg rounded-lg overflow-x-scroll scrollbar-hide scroll-smooth snap-y">
+            class="fixed top-[2%] right-[5%] w-[90%] h-[90%] bg-white text-black z-50 py-3 flex flex-col shadow-lg rounded-lg overflow-x-scroll scrollbar-hide scroll-smooth snap-y">
 
             <!-- Cart Content -->
             <div class="">
-                <div class="flex justify-between items-center px-6">
+                <div class="flex justify-between items-center px-3">
                     <div class="flex gap-2 items-center">
                         <div class="text-2xl font-bold">Cart</div><span class="bg-primary text-white size-3 flex justify-center items-center p-3 rounded-full text-xs">1</span>
                     </div>
@@ -252,6 +252,159 @@
         </div>
     </div>
     
+    <div >
+        <!-- Drawer Overlay -->
+        <div x-show="openHotline" x-transition.opacity class="fixed inset-0 bg-black/50 z-40" @click="openHotline = false"></div>
+
+        <!-- Hotline Content -->
+        <div x-show="openHotline" x-transition:enter="transition-transform transform duration-300"
+            x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0"
+            x-transition:leave="transition-transform transform duration-300"
+            x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full"
+            class="fixed top-[2%] right-[5%] w-[90%] h-[90%] bg-white text-black z-50 py-3 flex flex-col shadow-lg rounded-lg overflow-x-scroll scrollbar-hide scroll-smooth snap-y">
+
+            <!-- Cart Content -->
+            <div class="">
+                <div class="flex justify-between items-center px-3">
+                    <div class="flex gap-2 items-center">
+                        <div class="text-2xl font-bold">Call Our Hotlines</div>
+                    </div>
+                    <button @click="openHotline = false" class="self-end text-2xl text-gray-800">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+
+                <div class="text-gray-600 font-medium px-4 mt-4">
+                    Reach out to us through our hotlines for quick and reliable assistance.
+                </div>
+
+                <div class="px-4 mt-5">
+                    <div class="font-bold text-primary">Roces Branch</div>
+                    <div>
+                        <a href="tel:+6383761818" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path fill-rule="evenodd" d="m3.855 7.286 1.067-.534a1 1 0 0 0 .542-1.046l-.44-2.858A1 1 0 0 0 4.036 2H3a1 1 0 0 0-1 1v2c0 .709.082 1.4.238 2.062a9.012 9.012 0 0 0 6.7 6.7A9.024 9.024 0 0 0 11 14h2a1 1 0 0 0 1-1v-1.036a1 1 0 0 0-.848-.988l-2.858-.44a1 1 0 0 0-1.046.542l-.534 1.067a7.52 7.52 0 0 1-4.86-4.859Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">(+632) 8376-1818</div>
+                        </a>
+                        <a href="tel:+63287369664" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path fill-rule="evenodd" d="m3.855 7.286 1.067-.534a1 1 0 0 0 .542-1.046l-.44-2.858A1 1 0 0 0 4.036 2H3a1 1 0 0 0-1 1v2c0 .709.082 1.4.238 2.062a9.012 9.012 0 0 0 6.7 6.7A9.024 9.024 0 0 0 11 14h2a1 1 0 0 0 1-1v-1.036a1 1 0 0 0-.848-.988l-2.858-.44a1 1 0 0 0-1.046.542l-.534 1.067a7.52 7.52 0 0 1-4.86-4.859Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">(+632) 8736-9664</div>
+                        </a>
+                        <a href="tel:+637091-0395" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path fill-rule="evenodd" d="m3.855 7.286 1.067-.534a1 1 0 0 0 .542-1.046l-.44-2.858A1 1 0 0 0 4.036 2H3a1 1 0 0 0-1 1v2c0 .709.082 1.4.238 2.062a9.012 9.012 0 0 0 6.7 6.7A9.024 9.024 0 0 0 11 14h2a1 1 0 0 0 1-1v-1.036a1 1 0 0 0-.848-.988l-2.858-.44a1 1 0 0 0-1.046.542l-.534 1.067a7.52 7.52 0 0 1-4.86-4.859Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">Globe - 7091-0395</div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="px-4 mt-5">
+                    <div class="font-bold text-primary">Commonwealth Branch</div>
+                    <div>
+                        <a href="tel:+6389355095" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path fill-rule="evenodd" d="m3.855 7.286 1.067-.534a1 1 0 0 0 .542-1.046l-.44-2.858A1 1 0 0 0 4.036 2H3a1 1 0 0 0-1 1v2c0 .709.082 1.4.238 2.062a9.012 9.012 0 0 0 6.7 6.7A9.024 9.024 0 0 0 11 14h2a1 1 0 0 0 1-1v-1.036a1 1 0 0 0-.848-.988l-2.858-.44a1 1 0 0 0-1.046.542l-.534 1.067a7.52 7.52 0 0 1-4.86-4.859Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">(+632) 8935 5095</div>
+                        </a>
+                        <a href="tel:+63284019867" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path fill-rule="evenodd" d="m3.855 7.286 1.067-.534a1 1 0 0 0 .542-1.046l-.44-2.858A1 1 0 0 0 4.036 2H3a1 1 0 0 0-1 1v2c0 .709.082 1.4.238 2.062a9.012 9.012 0 0 0 6.7 6.7A9.024 9.024 0 0 0 11 14h2a1 1 0 0 0 1-1v-1.036a1 1 0 0 0-.848-.988l-2.858-.44a1 1 0 0 0-1.046.542l-.534 1.067a7.52 7.52 0 0 1-4.86-4.859Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">(+632) 8401 9867</div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="px-4 mt-5">
+                    <div class="font-bold text-primary">Marcos Highway Branch</div>
+                    <div>
+                        <a href="tel:+63286460871" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path fill-rule="evenodd" d="m3.855 7.286 1.067-.534a1 1 0 0 0 .542-1.046l-.44-2.858A1 1 0 0 0 4.036 2H3a1 1 0 0 0-1 1v2c0 .709.082 1.4.238 2.062a9.012 9.012 0 0 0 6.7 6.7A9.024 9.024 0 0 0 11 14h2a1 1 0 0 0 1-1v-1.036a1 1 0 0 0-.848-.988l-2.858-.44a1 1 0 0 0-1.046.542l-.534 1.067a7.52 7.52 0 0 1-4.86-4.859Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">(+632) 8646-0871</div>
+                        </a>
+                        <a href="tel:+63284019791" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path fill-rule="evenodd" d="m3.855 7.286 1.067-.534a1 1 0 0 0 .542-1.046l-.44-2.858A1 1 0 0 0 4.036 2H3a1 1 0 0 0-1 1v2c0 .709.082 1.4.238 2.062a9.012 9.012 0 0 0 6.7 6.7A9.024 9.024 0 0 0 11 14h2a1 1 0 0 0 1-1v-1.036a1 1 0 0 0-.848-.988l-2.858-.44a1 1 0 0 0-1.046.542l-.534 1.067a7.52 7.52 0 0 1-4.86-4.859Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">(+632) 8401 9791</div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="px-4 mt-5">
+                    <div class="font-bold text-primary">Pasig Branch</div>
+                    <div>
+                        <a href="tel:+63286719053" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path fill-rule="evenodd" d="m3.855 7.286 1.067-.534a1 1 0 0 0 .542-1.046l-.44-2.858A1 1 0 0 0 4.036 2H3a1 1 0 0 0-1 1v2c0 .709.082 1.4.238 2.062a9.012 9.012 0 0 0 6.7 6.7A9.024 9.024 0 0 0 11 14h2a1 1 0 0 0 1-1v-1.036a1 1 0 0 0-.848-.988l-2.858-.44a1 1 0 0 0-1.046.542l-.534 1.067a7.52 7.52 0 0 1-4.86-4.859Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">(+632) 8671-9053</div>
+                        </a>
+                        <a href="tel:+63279332961" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path fill-rule="evenodd" d="m3.855 7.286 1.067-.534a1 1 0 0 0 .542-1.046l-.44-2.858A1 1 0 0 0 4.036 2H3a1 1 0 0 0-1 1v2c0 .709.082 1.4.238 2.062a9.012 9.012 0 0 0 6.7 6.7A9.024 9.024 0 0 0 11 14h2a1 1 0 0 0 1-1v-1.036a1 1 0 0 0-.848-.988l-2.858-.44a1 1 0 0 0-1.046.542l-.534 1.067a7.52 7.52 0 0 1-4.86-4.859Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">Globe - 7933-2961</div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="px-4 mt-5">
+                    <div class="font-bold text-primary">BF Aguirre, Paranaque Branch</div>
+                    <div>
+                        <a href="tel:+63288610608" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path fill-rule="evenodd" d="m3.855 7.286 1.067-.534a1 1 0 0 0 .542-1.046l-.44-2.858A1 1 0 0 0 4.036 2H3a1 1 0 0 0-1 1v2c0 .709.082 1.4.238 2.062a9.012 9.012 0 0 0 6.7 6.7A9.024 9.024 0 0 0 11 14h2a1 1 0 0 0 1-1v-1.036a1 1 0 0 0-.848-.988l-2.858-.44a1 1 0 0 0-1.046.542l-.534 1.067a7.52 7.52 0 0 1-4.86-4.859Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">(+632) 8861-0608</div>
+                        </a>
+                        <a href="tel:+63285566741" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path fill-rule="evenodd" d="m3.855 7.286 1.067-.534a1 1 0 0 0 .542-1.046l-.44-2.858A1 1 0 0 0 4.036 2H3a1 1 0 0 0-1 1v2c0 .709.082 1.4.238 2.062a9.012 9.012 0 0 0 6.7 6.7A9.024 9.024 0 0 0 11 14h2a1 1 0 0 0 1-1v-1.036a1 1 0 0 0-.848-.988l-2.858-.44a1 1 0 0 0-1.046.542l-.534 1.067a7.52 7.52 0 0 1-4.86-4.859Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">(+632) 8556-6741</div>
+                        </a>
+                        <a href="tel:+639171685485" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path d="M7.25 11.5a.75.75 0 0 0 0 1.5h1.5a.75.75 0 0 0 0-1.5h-1.5Z" />
+                                <path fill-rule="evenodd" d="M6 1a2.5 2.5 0 0 0-2.5 2.5v9A2.5 2.5 0 0 0 6 15h4a2.5 2.5 0 0 0 2.5-2.5v-9A2.5 2.5 0 0 0 10 1H6Zm4 1.5h-.5V3a.5.5 0 0 1-.5.5H7a.5.5 0 0 1-.5-.5v-.5H6a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-9a1 1 0 0 0-1-1Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">Globe - 09171685485</div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="px-4 mt-5">
+                    <div class="font-bold text-primary">Cash and Carry, Makati Branch</div>
+                    <div>
+                        <a href="tel:+63284747558" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path fill-rule="evenodd" d="m3.855 7.286 1.067-.534a1 1 0 0 0 .542-1.046l-.44-2.858A1 1 0 0 0 4.036 2H3a1 1 0 0 0-1 1v2c0 .709.082 1.4.238 2.062a9.012 9.012 0 0 0 6.7 6.7A9.024 9.024 0 0 0 11 14h2a1 1 0 0 0 1-1v-1.036a1 1 0 0 0-.848-.988l-2.858-.44a1 1 0 0 0-1.046.542l-.534 1.067a7.52 7.52 0 0 1-4.86-4.859Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">(+632) 8474-7558</div>
+                        </a>
+                        <a href="tel:+639171685485" class="flex items-center gap-2 px-4 py-2 border-b border-gray-200">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                                <path d="M7.25 11.5a.75.75 0 0 0 0 1.5h1.5a.75.75 0 0 0 0-1.5h-1.5Z" />
+                                <path fill-rule="evenodd" d="M6 1a2.5 2.5 0 0 0-2.5 2.5v9A2.5 2.5 0 0 0 6 15h4a2.5 2.5 0 0 0 2.5-2.5v-9A2.5 2.5 0 0 0 10 1H6Zm4 1.5h-.5V3a.5.5 0 0 1-.5.5H7a.5.5 0 0 1-.5-.5v-.5H6a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-9a1 1 0 0 0-1-1Z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-base">Globe - 0917-1685485</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <main class="container">
         @yield('content')
     </main>
@@ -260,7 +413,7 @@
     <footer class="fixed bottom-0 left-0 w-full bg-orange-500 text-white flex justify-around p-3 text-sm font-bold z-50">
         <a href="#" class="text-center">MENU</a>
         <a href="#" class="text-center">LECHON PRICELIST</a>
-        <a href="#" class="text-center">HOTLINE</a>
+        <button @click="openHotline = true" class="text-center">HOTLINE</button>
     </footer>
 </body>
 </html>
--- resources/views/v2/home.blade.php
+++ resources/views/v2/home.blade.php
@@ -1,13 +1,51 @@
 @extends('layouts.guest')
 
 @section('content')
+    {{-- Marketing popup --}}
+    <div >
+        <!-- Drawer Overlay -->
+        <div x-show="marketingPopup" x-transition.opacity class="fixed inset-0 bg-black/50 z-40" @click="marketingPopup = false"></div>
+
+        <!-- Cart Drawer Content -->
+        <div x-show="marketingPopup" x-transition:enter="transition-transform transform duration-300"
+            x-transition:enter-start="-translate-y-full" x-transition:enter-end="translate-y-0"
+            x-transition:leave="transition-transform transform duration-300"
+            x-transition:leave-start="translate-y-0" x-transition:leave-end="-translate-y-full"
+            class="fixed top-[2%] right-[5%] w-[90%] max-w-[500px] pb-10 h-max bg-white text-black z-50 flex flex-col shadow-lg rounded-lg overflow-x-scroll scrollbar-hide scroll-smooth snap-y">
+
+            <!-- Content -->
+            <div class="w-full relative">
+                <div style="background-image: url('{{ asset('images/marketing-popup-hero.jfif') }}'); background-size: cover;" class="relative w-full h-[300px] rounded-lg overflow-hidden bg-bottom">
+
+                </div>
+
+                <!-- Close Button -->
+                <button @click="marketingPopup = false" class="absolute top-4 right-4 text-2xl text-gray-800">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class=" px-2 py-2 flex items-center justify-center bg-white rounded-full w-10 h-10 shadow-lg">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                </button>
+
+                <div class="font-cubao text-3xl text-left text-primary px-6 mt-5">
+                    Sign Up and Save 10% + Free Shipping on Your First Order!
+                </div>
+
+                <div class="font-medium px-6 text-left mt-5">Join us today and enjoy an exclusive 10% discount on your first purchase, plus free shipping! Sign up now to unlock these great perks and start shopping with savings.</div>
+
+                <div class="w-full px-6 mt-5">
+                    <button class="bg-primary text-white rounded-md  w-full px-6 py-4 mt-3 font-medium uppercase">Sign Up Now</button>
+                    <button class="bg-white border border-primary w-full text-primary rounded-md px-6 py-4 mt-3 font-medium uppercase">No, Thanks</button>
+                </div>
+            </div>
+        </div>
+    </div>
     {{-- Hero Section --}}
     <div class="pt-20 pb-16 flex flex-col items-center text-center relative h-[calc(100vh-15%)] overflow-hidden" style="background-image: url('{{ asset('/images/hero-bg.png') }}'); background-size: cover; background-position: center;">
         <div class="relative w-full h-full">
             <div class="inset-0 flex justify-center items-center text-green-800 font-bold px-10 font-cubao z-20 pt-20">
                 <h1 class="text-[55px] leading-[55px]">EVERYDAY<br>LECHON HAPPINESS</h1>
             </div>
-            <img src="{{ asset('/images/lechon-chopped.png') }}" alt="Lechon" class="w-full h-[400px] object-cover absolute bottom-0 left-0 z-10 scale-150">
+            <img @click="marketingPopup = true" src="{{ asset('/images/lechon-chopped.png') }}" alt="Lechon" class="w-full h-[400px] object-cover absolute bottom-0 left-0 z-10 scale-150">
         </div>
     </div>
     {{-- Delivery option --}}
