ecfcda48645708046f66ed7b70178e0305af72cc
randycph <randycph@gmail.com>
2025-04-21 13:29:25 +0800

    cart count

--- resources/views/components/navigation-component.blade.php
+++ resources/views/components/navigation-component.blade.php
@@ -1,10 +1,9 @@
 <nav 
 x-data="{
-
+    cartCount: 0,
     goback() {
         window.history.back();
     },
-
 }"  class="fixed max-w-8xl top-5 left-1/2 transform -translate-x-[50%] w-[90%] bg-green-700 px-4 flex justify-between items-center z-40 rounded-full">
     @if (isset($page) && $page == 'checkout')
         <div class="text-white font-bold text-lg py-3">
@@ -65,7 +64,7 @@ x-data="{
             </a>
             <button @click="openCart = true" class="text-white" aria-label="Cart" title="Cart">
                 <div class="relative">
-                    <span class="absolute -top-1 -right-1 bg-secondary text-black text-xs px-2 py-2 w-4 h-4 rounded-full flex items-center justify-center">3</span>
+                    <span x-show="cartCount > 0" class="absolute -top-1 -right-1 bg-secondary text-black text-xs px-2 py-2 w-4 h-4 rounded-full flex items-center justify-center" x-text="cartCount"></span>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10 text-white cursor-pointer hover:bg-primary-dark p-2 hover:rounded-full">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                     </svg>
