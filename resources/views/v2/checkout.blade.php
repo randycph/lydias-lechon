6b6bc7cc80c342400f20ef1558ef67663c369f77
randycph <randycph@gmail.com>
2025-05-13 13:18:07 +0800

    Update checkout.blade.php

--- resources/views/v2/checkout.blade.php
+++ resources/views/v2/checkout.blade.php
@@ -496,7 +496,7 @@
                             <div>
                                 <form
                                     x-data="{ isFormSubmitting: false }"
-                                    @submit="isFormSubmitting = true"
+                                    @submit="isFormSubmitting = true; setTimeout(() => { this.depositModal = true}, 3000)"
                                     action="{{ route('paymaya.pay') }}" method="POST" enctype="multipart/form-data" class="flex flex-col">
                                     @csrf
                                     <input type="hidden" name="sales_header_id" :value="paymentDetails.order_number">
