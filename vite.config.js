752673c5a4e90c43507515acff77a4eea54467b5
randycph <randycph@gmail.com>
2025-05-14 14:06:05 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue2'
 import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    base: '/lydias_v2/public/',
+    base: '/lydias_v2/build/',
     plugins: [
         laravel({
             input: [
