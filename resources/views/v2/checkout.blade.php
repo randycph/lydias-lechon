918d839ee136ec0bbd77bb1abbcfd7204e13a3dd
randycph <randycph@gmail.com>
2025-05-16 15:15:43 +0800

    Update checkout.blade.php

--- resources/views/v2/checkout.blade.php
+++ resources/views/v2/checkout.blade.php
@@ -266,8 +266,19 @@
                                                                     <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clip-rule="evenodd"/>
                                                                 </svg>
                                                             </div>
-                                                            <input @change="validateDeliveryDateTime(delivery)" 
-                                                            type="time" id="time" x-model="delivery.need_time" name="need_time" class="bg-gray-50 border leading-none border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " value="00:00" required />
+                                                            <select 
+                                                                type="time" id="time" x-model="delivery.need_time" name="need_time"
+                                                                @change="validateDeliveryDateTime(delivery)"
+                                                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                                            >
+                                                                <option value="">Select Hour</option>
+                                                                <template x-for="hour in 24" :key="hour">
+                                                                    <option :value="(hour < 10 ? '0' + hour : hour) + ':00'" 
+                                                                            x-text="(hour < 10 ? '0' + hour : hour) + ':00'">
+                                                                    </option>
+                                                                </template>
+                                                            </select>
+                                                        
                                                         </div>
                                                     </div>
                                                 </div>
