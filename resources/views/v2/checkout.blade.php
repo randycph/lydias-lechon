ea88f1a54cfce681097c747d89a60e346232c0d3
randycph <randycph@gmail.com>
2025-05-22 14:26:50 +0800

    Update checkout.blade.php

--- resources/views/v2/checkout.blade.php
+++ resources/views/v2/checkout.blade.php
@@ -562,7 +562,7 @@
                                             <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
                                                 ₱
                                             </span>
-                                            <input required name="amount" :value="paymentDetails.amount"  x-mask:dynamic="$money($input)" type="text" id="money" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full border-gray-300 p-2.5  " placeholder="">
+                                            <input required name="amount" :value="paymentDetails.amount" type="text" id="money" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full border-gray-300 p-2.5  " placeholder="">
                                         </div>
                                     </div>
                         
