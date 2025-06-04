4ce195c500eb648cd42648fd9e1a8565feb1e7cf
randycph <randycph@gmail.com>
2025-06-04 08:54:53 +0800

    hide dark classes

--- resources/views/v2/order-history.blade.php
+++ resources/views/v2/order-history.blade.php
@@ -525,7 +525,7 @@
                                 <div class="mt-4">
                                     <label class="font-semibold block mb-1">Amount to Pay:</label>
                                     <div class="flex">
-                                        <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                                        <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border rounded-e-0 border-gray-300 border-e-0 rounded-s-md">
                                             ₱
                                         </span>
                                         <input required name="amount" x-model="amount" type="text" id="money" class="rounded-none rounded-e-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full border-gray-300 p-2.5  " placeholder="">
