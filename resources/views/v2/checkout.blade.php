5604b8e7062542c93ef4a48ee443648d73bceff6
randycph <randycph@gmail.com>
2025-06-06 09:57:40 +0800

    Update checkout.blade.php

--- resources/views/v2/checkout.blade.php
+++ resources/views/v2/checkout.blade.php
@@ -65,7 +65,7 @@
                         <div class="flex gap-4 items-start w-full relative">
                             <div style="background-image: url('{{ asset('images/checkout-bg.png') }}')"
                                 class="w-20 h-20 min-w-20 min-h-20 object-cover overflow-hidden rounded-md bg-center">
-                                <img onerror="this.onerror=null;this.src='{{ asset('images/no-image.jpg') }}'" src="{{ asset('storage/products/' . $cart?->product?->photos[0]?->path) }}" alt="Checkout"
+                                <img onerror="this.onerror=null;this.src='{{ asset('images/no-image.jpg') }}'" src="{{ $cart?->product?->photos->first()?->path ? asset('storage/products/' . $cart?->product?->photos->first()->path) : asset('images/no-image.jpg') }}" alt="{{ $cart['name'] ?? $cart?->product?->name }}"
                                     class="w-20 h-20 object-cover">
                             </div>
                             <div class="flex flex-col">
