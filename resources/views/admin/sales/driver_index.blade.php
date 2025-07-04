b1570028707c20d6779dc6a6cbe3a0bc2ca73611
randycph <randycph@gmail.com>
2025-07-04 08:26:07 +0800

    remove dropdown for driver

--- resources/views/admin/sales/driver_index.blade.php
+++ resources/views/admin/sales/driver_index.blade.php
@@ -418,10 +418,6 @@
                             <label for="delivery_status">Status</label>
                             <select id="delivery_status" class="form-control mg-b-5" name="delivery_status"  data-width="100%" required="required">
                                 <option value="">- Select -</option>
-                                <option value="Open Date">Open Date</option>
-                                <option value="Scheduled for Processing">Scheduled for Processing</option>
-                                <option value="Processing">Processing</option>
-                                <option value="Ready For delivery">Ready For delivery</option>
                                 <option value="In Transit">In Transit</option>
                                 <option value="Delivered">Delivered</option>
                                 <option value="Returned">Returned</option>
