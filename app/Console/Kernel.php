d0f12e1146478e3226c05b60f9ef89772f7f0e05
Randy <evilryok@gmail.com>
2025-07-09 22:39:15 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('cart:check')->hourly();
-        $schedule->command('transactions:check-unpaid')->hourly();
+        // $schedule->command('cart:check')->hourly();
+        // $schedule->command('transactions:check-unpaid')->hourly();
         // $schedule->command('cron:test-email')->everyTenMinutes();
     }
 
