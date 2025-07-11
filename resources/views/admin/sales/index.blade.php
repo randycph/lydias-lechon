99a1b282fe6bdf40c75c1c292f43831e0f2704ba
randycph <randycph@gmail.com>
2025-07-11 09:17:52 +0800

    update

--- resources/views/admin/sales/index.blade.php
+++ resources/views/admin/sales/index.blade.php
@@ -359,8 +359,8 @@
                                                                 @endif
                                                                 <a class="dropdown-item" href="javascript:void(0);" onclick="show_delivery_history({{$sale->id}})" title="Order History" data-id="{{$sale->id}}">Show Order Status History</a>
                                                             
-                                                            @if (substr(strtolower($sale->user->email), 0, 8) == 'lydtemp_')
-                                                                <a class="dropdown-item" href="{{route('confirmation',$sale->HashOrderNumber)}}" target="_blank" title="View Guest Sales Summary" >Guest Sales Summary</a>
+                                                            @if (substr(strtolower($sale?->user?->email), 0, 8) == 'lydtemp_')
+                                                                <a class="dropdown-item" href="{{route('confirmation',$sale?->HashOrderNumber)}}" target="_blank" title="View Guest Sales Summary" >Guest Sales Summary</a>
                                                             @endif
 
 
