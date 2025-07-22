82e9db29b1feca30906a4c36b9bde447ed9ada33
randycph <randycph@gmail.com>
2025-07-22 13:48:10 +0800

    Update signup.blade.php

--- resources/views/v2/signup.blade.php
+++ resources/views/v2/signup.blade.php
@@ -620,11 +620,8 @@
                             </div>
 
                             <div class="flex justify-end p-4">
-                                <button @click="showModal = false, privacy = false" class="bg-gray-300 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-400 mr-2">
-                                    Decline
-                                </button>
-                                <button @click="agreePrivacy" class="bg-primary text-white px-4 py-2 rounded-md hover:bg-primary-dark">
-                                    Agree
+                                <button type="button" @click="agreePrivacy" class="bg-primary text-white px-4 py-2 rounded-md hover:bg-primary-dark">
+                                    I Agree
                                 </button>
                             </div>
                         </div>
