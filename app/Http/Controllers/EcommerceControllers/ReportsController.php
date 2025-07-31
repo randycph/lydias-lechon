cb78d9aae86d386004db9d8125c8ab79fb2aafa9
phpsqlcoder <phpsql.coder@gmail.com>
2025-07-31 14:56:22 +0800

    updated forecaster report

--- app/Http/Controllers/EcommerceControllers/ReportsController.php
+++ app/Http/Controllers/EcommerceControllers/ReportsController.php
@@ -343,29 +343,30 @@ class ReportsController extends Controller
             
             $tm_mrs=\App\Models\ProductDeliveryAddress::where('delivery_date','>=',date('Y-m-d',strtotime($tm_st)))->where('delivery_date','<=',date('Y-m-d',strtotime($tm_en)))
             ->get();
-            //dd($tm_mrs);
+            
             $ss = '';
             foreach($tm_mrs as $tm){
-                $obj = json_decode($tm->products)[0];
-                
-                DB::table('temp_mrs')->insert([
-                    'product_id' => $obj->product_id,
-                    'product_name' => $obj->product->name,
-                    'price' => $obj->product->price,
-                    'address' => $tm->address,
-                    'contact_person' => $tm->contact_person,
-                    'contact_tel' => $tm->contact_tel,
-                    'qty' => $obj->qty,
-                    'delivery_date' => $tm->delivery_date,
-                    'delivery_time' => $tm->delivery_time,
-                    'delivery_status' => $tm->delivery_status,
-                    'delivery_fee' => $tm->delivery_fee,
-                    'location' => $tm->location,
-                    'branch' => $tm->branch,
-                    'note' => $tm->note,
-                    'sales_header_id' => $tm->sales_header_id,
-                    'paella_price' => $tm->paella_price,
-                ]);
+                $obj2 = json_decode($tm->products);
+                foreach($obj2 as $obj){
+                    DB::table('temp_mrs')->insert([
+                        'product_id' => $obj->product_id,
+                        'product_name' => $obj->product->name,
+                        'price' => $obj->product->price,
+                        'address' => $tm->address,
+                        'contact_person' => $tm->contact_person,
+                        'contact_tel' => $tm->contact_tel,
+                        'qty' => $obj->qty,
+                        'delivery_date' => $tm->delivery_date,
+                        'delivery_time' => $tm->delivery_time,
+                        'delivery_status' => $tm->delivery_status,
+                        'delivery_fee' => $tm->delivery_fee,
+                        'location' => $tm->location,
+                        'branch' => $tm->branch,
+                        'note' => $tm->note,
+                        'sales_header_id' => $tm->sales_header_id,
+                        'paella_price' => $tm->paella_price,
+                    ]);
+                }
             }
             //dd(DB::select("select * from temp_mrs"));
         
