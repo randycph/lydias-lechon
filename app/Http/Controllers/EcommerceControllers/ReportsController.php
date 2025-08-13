8e1646a1683caf29afd6a50e934c2eb5855aeddd
randycph <randycph@gmail.com>
2025-08-13 08:11:35 +0800

    Update ReportsController.php

--- app/Http/Controllers/EcommerceControllers/ReportsController.php
+++ app/Http/Controllers/EcommerceControllers/ReportsController.php
@@ -369,7 +369,7 @@ class ReportsController extends Controller
                             'note' => $tm->note,
                             'sales_header_id' => $tm->sales_header_id,
                             'paella_price' => $tm->paella_price,
-                            'paella' => $obj?->paella,
+                            'paella' => isset($obj?->paella) ? $obj->paella : 0,
                         ]);
                     }
                 }
