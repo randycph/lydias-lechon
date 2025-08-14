a4ce44ca9b55fb2b25e3cfb28896010e9e515153
randycph <randycph@gmail.com>
2025-08-14 14:58:37 +0800

    Update view.blade.php

--- resources/views/admin/sales/view.blade.php
+++ resources/views/admin/sales/view.blade.php
@@ -72,7 +72,7 @@
                                     Delivery Charge: ₱{{ number_format($address->delivery_fee, 2) }}<br>
                                     Location: {{ $address->location }}<br>
                                     @if ($address->branch)
-                                    Delivery Branch: {{ $address->branch ? '' }}<br>
+                                    Delivery Branch: {{ $address->branch ?? '' }}<br>
                                     @endif
                                     Address {{ $k + 1 }}: {{ $address->address }}<br>
                                     @php
