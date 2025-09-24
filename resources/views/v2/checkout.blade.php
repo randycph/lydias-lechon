b4904b6e3047f5e9e8ccae745667cdb374ec039b
randycph <randycph@gmail.com>
2025-09-24 15:42:09 +0800

    Update checkout.blade.php

--- resources/views/v2/checkout.blade.php
+++ resources/views/v2/checkout.blade.php
@@ -616,7 +616,7 @@ $total += ($paella_price * $qty) + ($isFree ? 0 : ($price * $qty));
                                                         </div>
                                                         <div class="w-full lg:w-1/2">
                                                             <label class="font-bold block mb-1">Contact Number</label>
-                                                            <input type="tel" x-model="delivery.phone"
+                                                            <input type="tel" x-model="delivery.phone" pattern="^[0-9]{1,11}$" maxlength="13"
                                                                 @change="validateDeliveryAddress(delivery, 'phone', index)"
                                                                 class="w-full border border-gray-300 p-2 rounded-md"
                                                                 placeholder=""
@@ -769,7 +769,7 @@ $total += ($paella_price * $qty) + ($isFree ? 0 : ($price * $qty));
                                 <div class="my-2">
                                     <label for="mobile" class="block mb-2 text-sm font-bold text-gray-900">Mobile Number
                                         <span class="text-red-700">*</span></label>
-                                    <input type="tel" id="mobile" name="mobile"
+                                    <input type="tel" id="mobile" name="mobile" pattern="^[0-9]{1,11}$" maxlength="13"
                                         value="{{ auth()->check() ? auth()->user()->contact_mobile : '' }}"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
                                         required />
