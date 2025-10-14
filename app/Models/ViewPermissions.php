32a5c888f6dd4f38c1044987f4c282c1ccff6ddc
randycph <randycph@gmail.com>
2025-10-14 09:51:46 +0800

    Update ViewPermissions.php

--- app/Models/ViewPermissions.php
+++ app/Models/ViewPermissions.php
@@ -10,9 +10,9 @@ class ViewPermissions extends Model
 
     public static function check_permission($user_id,$action)
     {
-        if (auth()->user()->is_an_admin()) {
-            return 1;
-        }
+        // if (auth()->user()->is_an_admin()) {
+        //     return 1;
+        // }
 
         $rolepermission = ViewPermissions::where('role',$user_id)->first();
 
