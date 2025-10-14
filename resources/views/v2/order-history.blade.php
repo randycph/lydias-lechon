27feb53480e794c272e07ad5819b05763cfc2810
randycph <randycph@gmail.com>
2025-10-14 14:29:27 +0800

    Update order-history.blade.php

--- resources/views/v2/order-history.blade.php
+++ resources/views/v2/order-history.blade.php
@@ -316,8 +316,8 @@
                                                 class="flex gap-2 custom-btn btn-primary-dark group text-primary border hover:text-white border-primary bg-white hover:bg-primary-dark font-medium rounded-md w-full sm:w-auto px-5 py-3.5 text-center"
                                             >
                                             Track Order
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-primary group-hover:text-white">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 text-primary group-hover:text-white">
+                                                <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
                                             </svg>
                                             </button>
 
