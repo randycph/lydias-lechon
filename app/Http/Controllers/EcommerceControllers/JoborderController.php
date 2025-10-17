d8273d7a532fd93ac5e83dcc092c866493d6870f
randycph <randycph@gmail.com>
2025-10-17 16:07:04 +0800

    Update JoborderController.php

--- app/Http/Controllers/EcommerceControllers/JoborderController.php
+++ app/Http/Controllers/EcommerceControllers/JoborderController.php
@@ -255,6 +255,7 @@ class JoborderController extends Controller
     public function store(Request $request)
     {
         $rdata = $request->all();
+        // dd($rdata);
         if($request->totalcoupon > 0){
             $couponCode = $rdata['couponcode'];
             foreach($couponCode as $key => $coupon){
@@ -466,6 +467,7 @@ class JoborderController extends Controller
         }
         $sales = SalesHeader::whereId($salesHeader->id)->first();
         $total_payments = SalesPayment::where('sales_header_id',$salesHeader->id)->where('status','PAID')->sum('amount');
+        // dd($total_payments, $sales->gross_amount);
         if($total_payments >= $sales->gross_amount){
             $confirm_sales = SalesHeader::whereId($salesHeader->id)->update([
                 'isConfirm' => 1,
@@ -473,8 +475,10 @@ class JoborderController extends Controller
                 'confirm_remarks' => 'Auto confirm after payment completion',
                 'confirmed_on' => date('Y-m-d H:i:s')
             ]);
+        }
 
-            $sh = new SalesHeader();
+        $sh = new SalesHeader();
+        if ($sales->payment_status == 'PAID') {
             $sh->assign_to_production_branch($sales, $request->pb);
         }
 
