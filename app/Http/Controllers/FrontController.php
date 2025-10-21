e51322d80870e6e4eb200ced8692c24361da3ece
randycph <randycph@gmail.com>
2025-10-21 08:03:48 +0800

    Update FrontController.php

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -174,7 +174,7 @@ public function contact_us(Request $request)
                 'resume' => 'required|file|mimes:pdf,doc,docx|max:5120', // 5MB max
             ]);
 
-            $emailReceiver = 'orders@lydias-lechon.com';
+            $emailReceiver = Setting::info()->email ?? 'orders@lydias-lechon.com';
             $applicant = $request->all();
             $resume = $request->resume;
 
