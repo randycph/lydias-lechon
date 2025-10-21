98c50ade74f197dcf4c2fa51981e6b9468433531
randycph <randycph@gmail.com>
2025-10-21 09:48:48 +0800

    Update fixed-footer-component.blade.php

--- resources/views/components/fixed-footer-component.blade.php
+++ resources/views/components/fixed-footer-component.blade.php
@@ -1,6 +1,6 @@
 <!-- Fixed Bottom Navigation -->
 <footer class="fixed bottom-1 left-1/2 transform -translate-x-[50%] w-[90%] bg-tertiary text-white px-6 py-3 flex md:hidden justify-between items-center z-30 rounded-full">
-    <a href="{{ route('lechon-menu') }}" class="text-center">MENU</a>
-    <a href="{{ route('lechon-pricelist') }}" class="text-center">LECHON PRICELIST</a>
-    <button @click="openHotline = true" class="text-center">HOTLINE</button>
+    <a href="/menu" class="text-center">MENU</a>
+    <a href="/lechon-pricelist" class="text-center">LECHON PRICELIST</a>
+    <a href="/contact-us" class="text-center">CONTACT US</a>
 </footer>
