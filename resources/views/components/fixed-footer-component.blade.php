9e6e26be4976b2ea34f192d73f349cdb2ac76f84
randycph <randycph@gmail.com>
2025-10-21 09:53:02 +0800

    Update fixed-footer-component.blade.php

--- resources/views/components/fixed-footer-component.blade.php
+++ resources/views/components/fixed-footer-component.blade.php
@@ -1,5 +1,5 @@
 <!-- Fixed Bottom Navigation -->
-<footer class="fixed bottom-1 left-1/2 transform -translate-x-[50%] w-[90%] bg-tertiary text-white px-6 py-3 flex md:hidden justify-between items-center z-30 rounded-full">
+<footer class="fixed bottom-1 left-1/2 transform -translate-x-[50%] w-[90%] bg-tertiary text-white px-6 py-3 flex md:hidden justify-around items-center z-30 rounded-full">
     <a href="/menu" class="text-center">MENU</a>
     <a href="/lechon-pricelist" class="text-center">LECHON PRICELIST</a>
     {{-- <a href="/contact-us" class="text-center">CONTACT US</a> --}}
