ce2d97c5e4ea90900a383869ccc96fb300fc2255
randycph <randycph@gmail.com>
2025-10-24 11:20:38 +0800

    Update sidebar-menu2.blade.php

--- resources/views/admin/layouts/sidebar-menu2.blade.php
+++ resources/views/admin/layouts/sidebar-menu2.blade.php
@@ -43,7 +43,7 @@
             </li>
         @endif
 
-        @if (auth()->user()->has_access_to_module('forecaster'))
+        @if (auth()->user()->has_access_to_module('forecaster') || auth()->user()->has_access_to_route('forecaster.index'))
             <li class="nav-item @if (request()->routeIs('forecaster*')) active show @endif">
                 <a href="{{route('forecaster.index')}}" class="nav-link"><i data-feather="bar-chart"></i> <span>Forecaster @if (unreadForecastersTransactions() > 0)<span class="badge badge-light">{{ unreadForecastersTransactions() }}</span>@endif</span></a>
             </li>
