589c2308944b7631e887c874e1f82c566d5ddf24
randycph <randycph@gmail.com>
2025-11-04 16:14:18 +0800

    role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,7 +8,7 @@ class Role extends Model
 {
     public $table = 'role';
 
-    protected $fillable = [ 'name', 'description', 'created_by',];
+    protected $fillable = [ 'name', 'description', 'created_by', 'can_approve_payment', 'has_branches' ];
 
     public function is_admin() {
         return $this->id == 1;
