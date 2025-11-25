d714d8a71142d903044cddb6d1c04aba2fe1573a
randycph <randycph@gmail.com>
2025-11-25 10:24:00 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,14 +36,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(strpos(url()->current(), "storage") === FALSE && strpos(url()->current(), "theme") === FALSE){
-            // $insert_logs = \App\Models\ActivityLog::create([
-            //     'created_by' => \Auth::id() ?? 'guest',
-            //     'activity_type' => 'visit',
-            //     'dashboard_activity' => 'visit page',
-            //     'activity_desc' => \Request::ip(),
-            //     'activity_date' => date('Y-m-d H:i:s'),
-            //     'reference' => url()->current()
-            // ]);
+            $insert_logs = \App\Models\ActivityLog::create([
+                'created_by' => auth()->check() ? auth()->id() : 'guest',
+                'activity_type' => 'visit',
+                'dashboard_activity' => 'visit page',
+                'activity_desc' => \Request::ip(),
+                'activity_date' => date('Y-m-d H:i:s'),
+                'reference' => url()->current()
+            ]);
         }
         Paginator::defaultView('vendor.pagination.default');
         Blade::component('components.error', 'hasError');
