fec9440516f42e307c025fad2a22759d525c53b7
randycph <randycph@gmail.com>
2025-12-04 13:35:55 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -970,17 +970,17 @@ Route::get('driver', function() {
 
 Route::get('itextmo-sample', function() {
     try {
-        $ip = request()->number;
+        $number = request()->number;
         $message = request()->message;
 
-        if (!$ip || !$message) {
+        if (!$number || !$message) {
             return response()->json([
                 'message' => 'number and message parameters are required.'
             ], 400);
         }
 
         $sms = new \App\Services\ItextmoSmsService();
-        $result = $sms->sendWithCurl('09174128392', $message);
+        $result = $sms->sendWithCurl($number, $message);
 
         $response = json_decode($result, true);
 
