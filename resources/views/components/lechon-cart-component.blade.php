c68639e9cf084339296c5d85070aef66af007d2d
randycph <randycph@gmail.com>
2025-12-09 09:11:52 +0800

    add disable in add to cart

--- resources/views/components/lechon-cart-component.blade.php
+++ resources/views/components/lechon-cart-component.blade.php
@@ -278,8 +278,8 @@
                             Add to Cart
                         </button>
                         @else
-                        <button @click="added = true; add_to_cart('addcart', product.id, quantity, addons);"
-                            class="bg-primary primary-btn text-white w-full px-6 py-3 rounded-md font-semibold flex items-center justify-center gap-2 transition-all duration-300 ease-in-out custom-btn btn-primary">
+                        <button @click="added = true; add_to_cart('addcart', product.id, quantity, addons);" :disabled="loading" 
+                            class="bg-primary primary-btn text-white w-full px-6 py-3 rounded-md font-semibold flex items-center justify-center gap-2 transition-all duration-300 ease-in-out custom-btn btn-primary disabled:opacity-50 disabled:cursor-not-allowed">
                             <template x-if="!added">
                                 <span x-transition.opacity> Add to Cart </span>
                             </template>
