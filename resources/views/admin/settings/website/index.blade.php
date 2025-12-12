84ac9f99ba5273433b44b977b15b05f8c1e841a8
randycph <randycph@gmail.com>
2025-12-12 10:14:50 +0800

    Update index.blade.php

--- resources/views/admin/settings/website/index.blade.php
+++ resources/views/admin/settings/website/index.blade.php
@@ -657,7 +657,7 @@
         });
 
         // Populate time options
-        const timeSlots = ["05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00"];
+        const timeSlots = ["05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00"];
         timeSlots.forEach(time => {
             const option = new Option(time, time, false, false);
             $timeSelect.append(option);
