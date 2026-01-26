bc95c91c6bbbbf36457053cf5bf5d63f927dcb84
randycph <randycph@gmail.com>
2026-01-26 16:53:50 +0800

    confirmation remove pending and cancelled payments

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -319,9 +319,9 @@ class FrontendController extends Controller
         }
 
         $gc = GiftCertificate::where('sales_header_id',$id)->get();
-        $salesPayments = SalesPayment::where('sales_header_id',$id)->get();
+        $salesPayments = SalesPayment::where('sales_header_id',$id)->where('status', '!=', 'PENDING')->get();
         $salesDetails = SalesDetail::with('product.photos')->where('sales_header_id',$id)->get();
-        $totalPayment = SalesPayment::where('sales_header_id',$id)->sum('amount');
+        $totalPayment = SalesPayment::where('sales_header_id',$id)->where('status', '!=', 'PENDING')->sum('amount');
         $deliveries = DeliveryStatus::where('order_id',$id)->get();
         $totalNet = SalesHeader::where('id',$id)->sum('net_amount');
 
