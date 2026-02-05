b003d87d29711074f94ce3818ec802a06fbae45a
randycph <randycph@gmail.com>
2026-02-05 16:40:34 +0800

    Update index.blade.php

--- resources/views/admin/sales/index.blade.php
+++ resources/views/admin/sales/index.blade.php
@@ -400,16 +400,16 @@
                                                         <div class="dropdown-menu dropdown-menu-right">
                                                             <a class="dropdown-item" title="View Sales Summary" target="_blank" href="{{ route('sales-transaction.view',$sale->id) }}">View Sales Summary</a>
                                                             
+                                                                    <a class="dropdown-item"
+                                                                        href="{{ route('sales.update_details',$sale->id) }}"
+                                                                        title="Update Sales Details & Items">
+                                                                            Update Sales Details
+                                                                    </a>
                                                                 @if($sale->isConfirm != 1)
                                                                     @if(auth()->user()->has_access_to_route('sales.confirm.order'))
                                                                     <a class="dropdown-item"  href="javascript:void(0);" onclick="confirm_order({{$sale->id}},'{{ number_format((\App\EcommerceModel\SalesHeader::balance($sale->id)),2) }}');" title="Confirm Order" >Confirm Order</a>
                                                                     @endif
                                                                 @endif
-                                                                    <a class="dropdown-item"
-                                                                    href="{{ route('sales.update_details',$sale->id) }}"
-                                                                    title="Update Sales Details & Items">
-                                                                        Update Sales Details
-                                                                    </a>
                                                                 @if($dateneeded > date('Y-m-d H:i:s') && auth()->user()->has_access_to_route('sales-transaction.destroy'))
                                                                 <hr class="dropdown-divider">    
                                                                 <a class="dropdown-item text-danger" href="javascript:void(0)" onclick="delete_sales({{$sale->id}},'{{$sale->order_number}}')" title="Delete Transaction">Delete</a>
