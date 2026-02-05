fdc8566c5dfea3f3c2812307def43f2e8b979372
randycph <randycph@gmail.com>
2026-02-05 16:38:35 +0800

    Update index.blade.php

--- resources/views/admin/sales/index.blade.php
+++ resources/views/admin/sales/index.blade.php
@@ -405,7 +405,7 @@
                                                                     <a class="dropdown-item"  href="javascript:void(0);" onclick="confirm_order({{$sale->id}},'{{ number_format((\App\EcommerceModel\SalesHeader::balance($sale->id)),2) }}');" title="Confirm Order" >Confirm Order</a>
                                                                     @endif
                                                                 @endif
-                                                                @if (auth()->user()->has_access_to_route('sales-transaction.update'))
+                                                                @if (auth()->user()->has_access_to_route("sales-transaction.update"))
                                                                     <a class="dropdown-item"
                                                                     href="{{ route('sales.update_details',$sale->id) }}"
                                                                     title="Update Sales Details & Items">
