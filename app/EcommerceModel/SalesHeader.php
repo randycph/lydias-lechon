0e19d43a3962a4fbdc7fd9b039063f81ed76ac0b
randycph <randycph@gmail.com>
2026-02-09 13:54:22 +0800

    sync joborder status once sales order status is changed

--- app/EcommerceModel/SalesHeader.php
+++ app/EcommerceModel/SalesHeader.php
@@ -83,6 +83,7 @@ class SalesHeader extends Model
     public function assign_to_production_branch($sale, $pb){
         //dd($sale);
         $items = $sale->items;
+        $status = !empty($sale?->delivery_status) ? $sale->delivery_status : 'On Processed';
         foreach($items as $salesdetail){
 
             $current_total_order = JobOrder::whereDate('date_needed',date('Y-m-d',strtotime($salesdetail->delivery_date)))->count();
@@ -118,7 +119,7 @@ class SalesHeader extends Model
                 'delivery_tracking_number' => '',
                 'delivery_method' => $sale->delivery_type,
                 'pickup_branch' => $sale->outlet,
-                'delivery_status' => 'On Processed',
+                'delivery_status' => $status,
                 'status' => 'Active',
                 'jo_category' => 'Order',
                 'jo_order_type' => $sale->order_type ?? ' '
--- app/Http/Controllers/EcommerceControllers/SalesController.php
+++ app/Http/Controllers/EcommerceControllers/SalesController.php
@@ -1164,7 +1164,11 @@ class SalesController extends Controller
                     $deliveryAddress->save();
                 }
             } else {
-                SalesHeader::whereId($request->del_id)->update([
+                $sale = SalesHeader::whereId($request->del_id)->first();
+                $sale->delivery_status = $request->delivery_status;
+                $sale->save();
+
+                JobOrder::where('sales_number', $sale->order_number)->update([
                     'delivery_status' => $request->delivery_status
                 ]);
             }
