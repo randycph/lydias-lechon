1565d625a113ef017c90f45f970b1cf8e3b38f05
randycph <randycph@gmail.com>
2026-02-17 11:01:00 +0800

    Update Approvals.php

--- app/Models/Approvals.php
+++ app/Models/Approvals.php
@@ -7,15 +7,18 @@ use Illuminate\Support\Facades\Validator;
 
 class Approvals extends Model
 {
-    
-
     protected $table = 'approvals';
-    protected $fillable = ['approval_code', 'user_id', 'approval_type', 'reference_id', 'remarks'];
+    protected $fillable = [
+        'approval_code', 
+        'user_id', 
+        'approval_type', 
+        'reference_id', 
+        'remarks', 
+        'payment_id'
+    ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-
-   
 }
