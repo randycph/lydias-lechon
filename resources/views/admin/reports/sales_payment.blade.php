cc7a859d406373ee7aeaa9a67caabd9a66a40dcb
randycph <randycph@gmail.com>
2026-03-17 15:25:11 +0800

    Update sales_payment.blade.php

--- resources/views/admin/reports/sales_payment.blade.php
+++ resources/views/admin/reports/sales_payment.blade.php
@@ -213,7 +213,7 @@
                                     <td>{{date('Y-m-d',strtotime($r->hcreated))}}</td>
                                     <td>{{date('h:i A',strtotime($r->hcreated))}}</td>                                
                                     <td>{{date('Y-m-d',strtotime($r->payment_date ?? $r->pcreated))}}</td>
-                                    <td>{{date('h:i A',strtotime($r->payment_date ?? $r->pcreated))}}</td>
+                                    <td>{{date('h:i A',strtotime($r->updated_at ?? $r->pcreated))}}</td>
                                     <td>{{$r->customer_name}}</td>
                                     <td>{{$r->delivery_type}}</td>
                                     <th>
