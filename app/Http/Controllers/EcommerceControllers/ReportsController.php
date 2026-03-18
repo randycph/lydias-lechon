b3c424d756998104b0d75e2590d7a031ead39d73
randycph <randycph@gmail.com>
2026-03-18 15:24:09 +0800

    Update ReportsController.php

--- app/Http/Controllers/EcommerceControllers/ReportsController.php
+++ app/Http/Controllers/EcommerceControllers/ReportsController.php
@@ -1559,8 +1559,9 @@ class ReportsController extends Controller
                     })
                     ->whereNotNull('created_by')
                     ->whereHas('user', function ($query) {
-                        $query->where('user_type', 'cms')->where('role_id', '<>', env('CUSTOMER_ROLE_ID'));
+                        $query->where('user_type', 'cms');
                     })
+                    ->where('role', '<>', 'Customer')
                     ->whereNotNull('role')
                     ->when($module, function ($query) use ($module) {
                         $query->where('db_table', $module);
