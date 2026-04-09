ad31e0d7871d35e6cd9b577073a05f73341941cd
randycph <randycph@gmail.com>
2026-04-09 13:33:22 +0800

    Update index.blade.php

--- resources/views/admin/sales/index.blade.php
+++ resources/views/admin/sales/index.blade.php
@@ -253,35 +253,24 @@
                                     <td>{{ \Carbon\Carbon::parse($sale->created_at)->format('Y-m-d g:i A') }}</td>
                                     <td>
                                         @if($sale->delivery_status <> 'Open Date')
+                                            @php
+                                                $dates = [];
 
-                                            @if ($sale->deliveryAddress && count($sale->deliveryAddress) > 0)
-                                                @php
-                                                    $dateneeded = '';
+                                                foreach ($sale->deliveryAddress as $address) {
+                                                    $rawDateTime = trim($address->delivery_date . ' ' . $address->delivery_time);
 
-                                                    foreach ($sale->deliveryAddress as $address) {
-                                                        $rawDateTime = trim($address->delivery_date . ' ' . $address->delivery_time);
-
-                                                        // Skip invalid zero dates
-                                                        if (
-                                                            $address->delivery_date == '0000-00-00' ||
-                                                            $address->delivery_date == '0000-00-00 00:00:00' ||
-                                                            $rawDateTime == '0000-00-00 00:00:00'
-                                                        ) {
-                                                            continue;
-                                                        }
-
-                                                        if ($dateneeded != '') {
-                                                            $dateneeded .= ', ';
-                                                        }
-
-                                                        $dateneeded .= \Carbon\Carbon::parse($rawDateTime)->format('Y-m-d g:i A');
+                                                    if (
+                                                        empty($address->delivery_date) ||
+                                                        str_starts_with($address->delivery_date, '0000-00-00')
+                                                    ) {
+                                                        continue;
                                                     }
-                                                @endphp
 
-                                                {{ $dateneeded }}
-                                            @else
-                                                {{ $dateneeded }}
-                                            @endif
+                                                    $dates[] = \Carbon\Carbon::parse($rawDateTime)->format('Y-m-d g:i A');
+                                                }
+                                            @endphp
+
+                                            {{ implode(', ', $dates) }}
                                         @endif
                                     </td>
                                     <td>{{ $sale->delivery_type }}</td>
