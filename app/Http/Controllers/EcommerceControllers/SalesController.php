80aeab3274cd787790f0fa21cab2e9c130070110
randycph <randycph@gmail.com>
2026-04-10 14:35:11 +0800

    fix price showing

--- app/Http/Controllers/EcommerceControllers/SalesController.php
+++ app/Http/Controllers/EcommerceControllers/SalesController.php
@@ -991,14 +991,17 @@ class SalesController extends Controller
             return redirect()->route('sales-transaction.index')->with('error', 'Sales record not found.');
         }
 
-        if ($sales->is_sub == 1) {
-            $subSales = SalesHeader::where('id', $sales->parent_sales_header_id)->first();
-            $salesPayments = $subSales->payments ?? collect();
-            $totalPayment = SalesPayment::where('sales_header_id', $sales->parent_sales_header_id)->sum('amount');
-        } else {
-            $salesPayments = SalesPayment::where('sales_header_id',$id)->get();
-            $totalPayment = SalesPayment::where('sales_header_id',$id)->sum('amount');
-        }
+        // if ($sales->is_sub == 1) {
+        //     $subSales = SalesHeader::where('id', $sales->parent_sales_header_id)->first();
+        //     $salesPayments = $subSales->payments ?? collect();
+        //     $totalPayment = SalesPayment::where('sales_header_id', $sales->parent_sales_header_id)->sum('amount');
+        // } else {
+        //     $salesPayments = SalesPayment::where('sales_header_id',$id)->get();
+        //     $totalPayment = SalesPayment::where('sales_header_id',$id)->sum('amount');
+        // }
+
+        $salesPayments = SalesPayment::where('sales_header_id',$id)->get();
+        $totalPayment = SalesPayment::where('sales_header_id',$id)->sum('amount');
 
         if (!$sales) {
             return redirect()->route('sales-transaction.index')->with('error', 'Sales record not found.');
