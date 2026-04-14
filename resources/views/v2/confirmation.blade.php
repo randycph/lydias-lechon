04e6cdc2a1461704381053cc86fd5ee03cbdf802
Randy <evilryok@gmail.com>
2026-04-14 20:50:28 +0800

    disable pay now

--- resources/views/v2/confirmation.blade.php
+++ resources/views/v2/confirmation.blade.php
@@ -31,7 +31,7 @@
                     </div>
                 @else
                     @if (strtolower($sales->PaymentStatus) != 'paid' && $sales->status != 'CANCELLED')
-                    <button @click="openPaymentModal({{$sales->net_amount}}, '{{ $sales->id }}')" type="button"
+                    <button {{ $isExpired ? 'disabled' : '' }} @click="openPaymentModal({{$sales->net_amount}}, '{{ $sales->id }}')" type="button"
                         class="border {{ $sales->status == 'CANCELLED' ? 'hidden' : '' }} border-primary text-primary px-6 py-4 w-full text-center rounded-md hover:bg-primary hover:text-white transition-colors duration-300">
                         Pay Now
                     </button>
