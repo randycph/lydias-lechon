72131b5daba5e0e986a19caa2e1f6c25b1149cd7
randycph <randycph@gmail.com>
2026-04-15 15:20:19 +0800

    Update SalesHeader.php

--- app/EcommerceModel/SalesHeader.php
+++ app/EcommerceModel/SalesHeader.php
@@ -44,6 +44,7 @@ class SalesHeader extends Model
         'net_amount',
         'discount_amount',
         'payment_status',
+        'region',
         'province',
         'city',
         'barangay',
