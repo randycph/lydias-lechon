71fbc2e3c58ef7c915d906ced0f9b634f6157d31
randycph <randycph@gmail.com>
2026-04-20 13:53:32 +0800

    Update driver_index.blade.php

--- resources/views/admin/sales/driver_index.blade.php
+++ resources/views/admin/sales/driver_index.blade.php
@@ -230,7 +230,7 @@
 
                                     <td style="display:none;">{{ rtrim($payment_types,",") }}</td>
                                     <td>
-                                        {{ number_format(($sale->items->sum('net_amount') + ($sale->delivery_fee_amount ?? 0)) - $sale->payments->where('is_discount', 1)->where('status', 'PAID')->sum('amount'), 2) }}
+                                        {{ number_format(($sale?->items?->sum('net_amount') + ($sale->delivery_fee_amount ?? 0)) - $sale->payments->where('is_discount', 1)->where('status', 'PAID')->sum('amount'), 2) }}
                                     </td>
                                      <td>{{ $sale['type'] == 'job' ? 'Job Order' : 'Sales' }}</td>
                                     <td width="10%">
