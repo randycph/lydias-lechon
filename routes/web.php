b3d359d626b3fddabd9e84fbdcf24b22aad68ba2
wsiarcalaalvin <arcala.alvin@webfocus.ph>
2026-04-21 11:20:18 +0800

    additional lane for the store coupon

--- routes/web.php
+++ routes/web.php
@@ -699,7 +699,7 @@ Route::group(['middleware' => ['authenticated', 'cmsUserOnly']], function () {
 
     // Coupon
     Route::resource('/admin/coupons','EcommerceControllers\CouponController');
-    Route::post('/insert_coupon', [CouponController::class, 'insert_coupons'])->name('coupon.insert');
+    Route::post('/admin/coupon/store','EcommerceControllers\CouponController@store')->name('coupons.store');
     Route::get('/admin/coupon/{id}/{status}', 'EcommerceControllers\CouponController@update_status')->name('coupon.change-status');
     Route::post('/admin/coupon-single-delete', 'EcommerceControllers\CouponController@single_delete')->name('coupon.single.delete');
     Route::get('/admin/coupon-restore/{id}', 'EcommerceControllers\CouponController@restore')->name('coupon.restore');
