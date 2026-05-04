d445ae74fdab728fd7102e2578cb1448c90d7d5c
randycph <randycph@gmail.com>
2026-05-04 16:48:00 +0800

    Update sales.blade.php

--- resources/views/admin/reports/sales.blade.php
+++ resources/views/admin/reports/sales.blade.php
@@ -300,8 +300,8 @@
                                     <th>{{$r->order_source}}</th>
                                     <th>{{$r->jnum}}</th>
                                     <th>{{$r->prod_branch}}</th>
-                                    <td>{{date('Y-m-d',strtotime($r->delivery_date))}}</td>
-                                    <td>{{date('h:i A',strtotime($r->delivery_date))}}</td>
+                                    <td>{{$r->delivery_status == 'Open Date' ? '' : date('Y-m-d',strtotime($r->delivery_date))}}</td>
+                                    <td>{{$r->delivery_status == 'Open Date' ? '' : date('H:i',strtotime($r->delivery_date))}}</td>
                                     <td>{{number_format($r->price,2)}}</td>
                                     <td>{{number_format($r->qty,2)}}</td>
                                     <td>{{number_format((($r->price + $r->paella_price) * $r->qty),2)}}</td>
