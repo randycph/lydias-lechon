20429cb4df79f8a71f9ec9a3d45e48d49707c1b1
wsiarcalaalvin <arcala.alvin@webfocus.ph>
2026-05-15 14:34:32 +0800

    changes in coupon reports

--- resources/views/admin/reports/coupon/list.blade.php
+++ resources/views/admin/reports/coupon/list.blade.php
@@ -12,7 +12,18 @@
     <link href="{{ asset('lib/bselect/dist/css/bootstrap-select.css') }}" rel="stylesheet">
     <link href="{{ asset('lib/ion-rangeslider/css/ion.rangeSlider.min.css') }}" rel="stylesheet">
 
+    <!-- DashForge CSS -->
+    <link rel="stylesheet" href="{{ asset('css/dashforge.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dashforge.dashboard.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dashforge.demo.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/skin.deepblue.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/custom-admin.css') }}">
+
     <style>
+        @page {
+            size: auto;
+        }
+
         .row-selected {
             background-color: #92b7da !important;
         }
@@ -21,142 +32,175 @@
 
 @section('content')
 
-  
-                    
-               
-<div style="margin:0px 40px 200px 40px;font-family:Arial;">
-         <h4 class="mg-b-0 tx-spacing--1">Sales Report</h4>
-                    <form action="{{route('report.coupon.list')}}" method="get">
-                        <input type="hidden" name="act" value="go">
-                        @csrf
-                        <table style="font-size:12px;">
-                            <tr>
-                                <td>Coupon Code</td>
-                                <td>Order Start date</td>
-                                <td>Order End Date</td>
-                                <td>Category</td>
-                                <td>Brand</td>
-                                <td>Product</td>
-                                <td>Customer</td>
-                                <td>&nbsp;</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <input style="font-size:12px;width: 140px;" type="text" class="form-control input-sm" name="coupon_code" autocomplete="off"
-                                    @if(isset($_GET['coupon_code'])) value="{{$_GET['coupon_code']}}" @endif >
-                                </td>
-                                <td><input style="font-size:12px;width: 140px;" type="date" class="form-control input-sm" name="start" autocomplete="off"
-                                    @if(isset($_GET['start'])) value="{{$_GET['start']}}" @endif >
-                                </td>
-                                <td><input style="font-size:12px;width: 140px;" type="date" class="form-control input-sm" name="end" autocomplete="off"
-                                    @if(isset($_GET['end'])) value="{{$_GET['end']}}" @endif >
-                                </td>
-                                <td>
-                                    <select style="font-size:12px;width: 140px;" name="category" id="category" class="form-control input-sm">
-                                        <option value="">Select</option>
-                                        @php
-                                            $categories = \App\Models\ProductCategory::orderBy('name')->get();
-                                        @endphp
-                                        @forelse($categories as $c)
-                                            <option value="{{$c->id}}" 
-                                                @if(isset($_GET['category']) and $_GET['category']==$c->id) selected="selected" @endif 
-                                            >
-                                                {{$c->name}}
-                                            </option>
-                                        @empty
-                                        @endforelse
-                                    </select>
-                                </td>
-                                <td>
-                                    <select style="font-size:12px;width: 140px;" name="brand" id="brand" class="form-control input-sm">
-                                        <option value="">Select</option>
-                                        @php
-                                            $brands = \App\Models\Product::distinct()->get();
-                                        @endphp
-                                        @forelse($brands as $b)
-                                            <option value="{{$b->brand}}"
-                                                @if(isset($_GET['brand']) and $_GET['brand']==$b->brand) selected="selected" @endif 
-                                            >
-                                                {{$b->brand}}
-                                            </option>
-                                        @empty
-                                        @endforelse
-                                    </select>
-                                </td>
-                                <td>
-                                    <select style="font-size:12px;width: 140px;" name="product" id="product" class="form-control input-sm">
-                                        <option value="">Select</option>
-                                        @php
-                                            $products = \App\Models\Product::orderBy('name')->get();
-                                        @endphp
-                                        @forelse($products as $p)
-                                            <option value="{{$p->id}}"
-                                                @if(isset($_GET['product']) and $_GET['product']==$p->id) selected="selected" @endif 
-                                            >
-                                                {{$p->name}}
-                                            </option>
-                                        @empty
-                                        @endforelse
-                                    </select>
-                                </td>
-                                <td>
-                                    <select style="font-size:12px;width: 140px;" name="customer" id="customer" class="form-control input-sm">
-                                        <option value="">Select</option>
-                                        @php
-                                            $customers = \App\Models\User::where('is_active', 1)->where('role_id','6')->orderBy('name')->get();
-                                        @endphp
-                                        @forelse($customers as $cu)
-                                            <option value="{{$cu->id}}"
-                                                @if(isset($_GET['customer']) and $_GET['customer']==$cu->id) selected="selected" @endif 
-                                            >
-                                                {{$cu->name}}
-                                            </option>
-                                        @empty
-                                        @endforelse
-                                    </select>
-                                </td>
-                                <td><button type="submit" class="btn btn-primary" style="margin:0px 0px 0px 20px;">Generate</button></td>
-                                <td><a href="{{ route('report.sales.list') }}" class="btn btn-success" style="margin:0px 0px 0px 20px;">Reset</a></td>
-                            </tr>
-                        </table>
-                    </form>
-            
-            @if($rs <>'')
-              
-                        <br><br>
-                        <table id="example" class="display nowrap" style="width:100%;font: normal 13px/150% Arial, sans-serif, Helvetica;">
-                            <thead>
-                            <tr>
-                                <th align="left">Code</th>
-                                <th align="left">Name</th>
-                                <th align="left">Order #</th>
-                                <th align="left">Customer</th>
-                                <th align="left">Total Amount</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @forelse($rs as $r)
-                                <tr>
-                                    <td>{{$r->coupon_code}}</td>
-                                    <td>{{$r->name}}</td>
-                                    <th>{{$r->order_number}}</th>
-                                    <td>{{$r->customer_name}}</td>
-                                    <td>{{number_format($r->net_amount,2)}}</td>
-                                </tr>
-                            @empty
-                                <tr>
-                                    <td colspan="5">No report result.</td>
-                                </tr>
-                            @endforelse
+<div class="container">
+    <div class="text-center mg-b-10">
+        <img height="100px" src="{{ asset('images/lydias1965.png') }}" alt="">
+        <h4 class="mg-b-0 tx-spacing--1">Coupon Usage Report</h4>
+    </div>
+    <br>
 
-                            </tbody>
+    <div class="row row-sm">
+        <div class="col-md-12">
+            <form action="{{route('report.coupon.list')}}" method="get">
+                <input type="hidden" name="act" value="go">
+                @csrf
 
-                        </table>
-                   
-            @endif
+                <table width="100%" style="font-size:12px;font-family:Arial;">
+                <tr>
+                    <td>Coupon Code</td>
+                    <td>Type of Coupon</td>
+                    <td>Order Source</td>
+                    <td>Order Start date</td>
+                    <td>Order End Date</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                    <tr>
+                        <td>
+                            <input style="font-size:12px;width: 140px;" type="text" class="form-control input-sm" name="coupon_code" autocomplete="off"
+                            @if(isset($_GET['coupon_code'])) value="{{$_GET['coupon_code']}}" @endif >
+                        </td>
+                        <td>
+                        <select style="font-size:12px;width: 160px;"
+                                name="coupon_type"
+                                class="form-control input-sm">
+                            <option value="">All</option>
 
+                            <option value="free-shipping-optn"
+                                @if(isset($_GET['coupon_type']) && $_GET['coupon_type'] == 'free-shipping-optn') selected @endif>
+                                Free Shipping
+                            </option>
+
+                            <option value="discount-amount-optn"
+                                @if(isset($_GET['coupon_type']) && $_GET['coupon_type'] == 'discount-amount-optn') selected @endif>
+                                Amount Discount
+                            </option>
+
+                            <option value="discount-percentage-optn"
+                                @if(isset($_GET['coupon_type']) && $_GET['coupon_type'] == 'discount-percentage-optn') selected @endif>
+                                Percentage Discount
+                            </option>
+
+                            <option value="free-product-optn"
+                                @if(isset($_GET['coupon_type']) && $_GET['coupon_type'] == 'free-product-optn') selected @endif>
+                                Free Product
+                            </option>
+                        </select>
+                    </td>
+                        <td>
+                <select style="font-size:12px;width: 140px;"
+                        name="order_source"
+                        class="form-control input-sm">
+                    <option value="">All</option>
+                    <option value="web" @if(isset($_GET['order_source']) && $_GET['order_source'] == 'web') selected @endif>
+                        Web
+                    </option>
+                    <option value="jo" @if(isset($_GET['order_source']) && $_GET['order_source'] == 'jo') selected @endif>
+                        JO
+                    </option>
+                </select>
+            </td>
+
+                        <td>
+                            <input style="font-size:12px;width: 140px;" type="date" class="form-control input-sm" name="start" autocomplete="off"
+                            @if(isset($_GET['start'])) value="{{$_GET['start']}}" @endif >
+                        </td>
+
+                        <td>
+                            <input style="font-size:12px;width: 140px;" type="date" class="form-control input-sm" name="end" autocomplete="off"
+                            @if(isset($_GET['end'])) value="{{$_GET['end']}}" @endif >
+                        </td>
+                        
+                        <td>
+                            <button type="submit" class="btn btn-primary btn-sm" style="margin:0px 0px 0px 20px;">Generate</button>
+                        </td>
+
+                        <td>
+                            <a href="{{ route('report.sales.list') }}" class="btn btn-success btn-sm" style="margin:0px 0px 0px 20px;">Reset</a>
+                        </td>
+                    </tr>
+                </table>
+            </form>
         </div>
-    
+    </div>
+
+    @if($rs <>'')
+        <br><br>
+
+        <div class="row row-sm">
+            <div class="col-md-12">
+                <table id="example" class="display nowrap" style="width:100%;font: normal 13px/150% Arial, sans-serif, Helvetica;">
+                    <thead>
+                        <tr>
+                            <th align="left">ID</th>
+                            <th align="left">Code</th>
+                            <th align="left">Name</th>
+                            <th align="left">Reward</th>
+                            <th align="left">Order Source</th>
+                            <th align="left">Order #</th>
+                            <th align="left">Customer</th>
+                            <th align="left">Coupon Amount</th>
+                            <th align="left">Total Amount</th>
+                            <th align="left">Order Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                        $fsCount = 0;
+                        $adCount = 0;
+                        $pdCount = 0;
+                        $fpCount = 0;
+                    @endphp
+                       @forelse($rs as $r)
+                        @php
+                            $displayId = '';
+
+                            if ($r->reward == 'free-shipping-optn') {
+                                $fsCount++;
+                                $displayId = 'FS-' . $fsCount;
+                                $rewardName = 'Free Shipping';
+                            } elseif ($r->reward == 'discount-amount-optn') {
+                                $adCount++;
+                                $displayId = 'AD-' . $adCount;
+                                $rewardName = 'Amount Discount';
+                            } elseif ($r->reward == 'discount-percentage-optn') {
+                                $pdCount++;
+                                $displayId = 'PD-' . $pdCount;
+                                $rewardName = 'Percentage Discount';
+                            } elseif ($r->reward == 'free-product-optn') {
+                                $fpCount++;
+                                $displayId = 'FP-' . $fpCount;
+                                $rewardName = 'Free Product';
+                            } else {
+                                $displayId = 'CP-' . ($loop->iteration);
+                                $rewardName = $r->reward;
+                            }
+                        @endphp
+
+                        <tr>
+                            <td>{{ $displayId }}</td>
+                            <td>{{ $r->coupon_code }}</td>
+                            <td>{{ $r->name }}</td>
+                            <td>{{ $rewardName }}</td>
+                            <td>{{ $r->order_source_label  }}</td>
+                            <td>{{ $r->order_number }}</td>
+                            <td>{{ $r->customer_name }}</td>
+                            <td>₱{{ number_format($r->coupon_amount ?? 0, 2) }}</td>
+                            <td>₱{{ number_format($r->net_amount ?? 0, 2) }}</td>
+                            <td>{{ $r->order_date}}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="8">No report result.</td>
+                        </tr>
+                    @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
+
+</div>
 
 @endsection
 
@@ -165,19 +209,17 @@
     <script src="{{ asset('lib/bselect/dist/js/i18n/defaults-en_US.js') }}"></script>
     <script src="{{ asset('lib/prismjs/prism.js') }}"></script>
     <script src="{{ asset('lib/jqueryui/jquery-ui.min.js') }}"></script>
-
 @endsection
 
 @section('customjs')
 <script src="{{ asset('js/datatables/Buttons-1.6.1/js/buttons.colVis.min.js') }}"></script>
-<script>
-  
 
+<script>
     $(document).ready(function() {
-        $('#example').DataTable( {
+        $('#example').DataTable({
             dom: 'Bfrtip',
             pageLength: 20,
-            order: [[0,'desc']],
+            order: [[0, 'desc']],
             buttons: [
                 {
                     extend: 'print',
@@ -203,13 +245,7 @@
                         columns: ':visible'
                     }
                 },
-                 // {
-                //     extend: 'pdf',
-                //     exportOptions: {
-                //         columns: ':visible'
-                //     }
-                // },
-                {   
+                {
                     extend: 'pdfHtml5',
                     text: 'PDF',
                     exportOptions: {
@@ -217,19 +253,12 @@
                             page: 'current'
                         }
                     },
-                    orientation : 'landscape',
-                    pageSize : 'LEGAL'
+                    orientation: 'landscape',
+                    pageSize: 'LEGAL'
                 },
                 'colvis'
-            ],
-            columnDefs: [ {
-                targets: [6,10,11,12,13,14,15,16,17,18,19],
-                visible: false
-            } ]
-        } );
-    } );
+            ]
+        });
+    });
 </script>
 @endsection
-
-
-
